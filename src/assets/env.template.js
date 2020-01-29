f8e78219a5cc96ab0f6f46e0db03642f51f7a15d
Felix Riess <felix.riess@exxcellent.de>
2020-01-29 10:10:25 +0100

    fix typo

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -4,5 +4,5 @@
     // Environment variables
     window["env"]["countryServiceUrl"] = "${COUNTRY_SERVICE_URL}";
     window["env"]["currencySerivceUrl"] = "${CURRENCY_SERVICE_URL}";
-    window["env"]["lanugageServiceUrl"] = "${LANGUAGE_SERVICE_URL}";
+    window["env"]["languageServiceUrl"] = "${LANGUAGE_SERVICE_URL}";
 })(this);
